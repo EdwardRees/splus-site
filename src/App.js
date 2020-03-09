e95dde0a375bfb0855fb30e753bcd7f914353cf1
Edward Rees <erees.hk@gmail.com>
2020-03-08 22:08:08 -0700

    addition of 404 not found page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,15 @@ import "./App.css";
 import React, { Fragment } from "react";
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import { Logo, Header, Footer } from "./components/";
-import { Us, Design, FacilitiesManagement, ActiveSeniorLiving, Lifestyle, Enquiry } from "./views";
+import {
+  Us,
+  Design,
+  FacilitiesManagement,
+  ActiveSeniorLiving,
+  Lifestyle,
+  Enquiry,
+  Error404
+} from "./views";
 
 const InProgress = () => {
   return (
@@ -28,7 +36,7 @@ const InProgress = () => {
 
 const RoutedApp = () => (
   // <div style={{ display: 'flex', height: "100vh", flexDirection: 'column' }}>
-  <div style={{display: 'flex', flexDirection: "column", height: "100vh"}}>
+  <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
     <Router>
       <Route path="/" component={InProgress} exact />
       <Route path="/dev">
@@ -36,11 +44,20 @@ const RoutedApp = () => (
         <Switch>
           <Route path="/dev/us" component={Us} />
           <Route path="/dev/wip/design" component={Design} />
-          <Route path="/dev/wip/facilities-management" component={FacilitiesManagement} />
-          <Route path="/dev/wip/active-senior-living" component={ActiveSeniorLiving} />
+          <Route
+            path="/dev/wip/facilities-management"
+            component={FacilitiesManagement}
+          />
+          <Route
+            path="/dev/wip/active-senior-living"
+            component={ActiveSeniorLiving}
+          />
           <Route path="/dev/wip/lifestyle" component={Lifestyle} />
           <Route path="/dev/wip/enquiry" component={Enquiry} />
         </Switch>
+        <Route>
+          <Error404 name="Not Found" />
+        </Route>
         <Footer />
       </Route>
     </Router>
